b696867e5e3962a692e1df32beaa7d8f16df1fa2
IanMichelsen117 <138816928+IanMichelsen117@users.noreply.github.com>
2023-10-09 18:09:00 +0000

    Feature: Metodos de Vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
-<script setup>
+<script setup> 
+import { ref } from 'vue'; 
+const header = ref('App Lista de compras'); 
+const items = ref([ 
+  {id: 1, label: '10 bolillos'},
+  {id: 2, label: '1 lata de frijoles'}, 
+  {id: 3, label: '2 lata de atún'} ]); 
+  //Agregando metodo para guardar nuevo articulo en la lista
+const saveItem=() =>{
+items.value.push({id: items.value.length + 1, label: newItem.value})
+//Limpiando el contenido de newItem
+newItem.value="";
+};
+const newItem = ref(''); 
+const newItemHighPriority = ref(false); 
+</script> 
 
-import { ref } from 'vue';
-const header = ref('App Lista de Compras');
-const items = ref([
-    {id: 1, label: '10 bolillos'},
-    {id: 2, label: '1 lata de frijoles'},
-    {id: 3, label: '2 lata de atún'},
-]);
-const newitem=ref('1 bolsa de te');
-const newitemhighpriority=ref(false);
-</script>
+<template> <form v-on:submit.prevent="saveItem" class="add-item form"> 
+<!-- Input de Nuevo Articulo --> 
+<input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo"> 
+<!-- Check Boxes --> 
+<label> 
+  <input v-model="newItemHighPriority" type="checkbox"> Alta Prioridad 
+  </label> 
+  {{ newItemHighPriority ? "🔥" : "🧊" }} 
+  <!-- Boton de UI --> 
+  <button class="btn btn-primary">Salvar Articulo
 
-<template>
-  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-  <input v-model.trim="newitem" placeholder="Ingresar asrticulo">
-  <!-- check boxes -->
-<leabel>
-  <input v-model="newitemhighpriority" type="checkbox">
-  Alta preoridad
-</leabel>
-  {{ newitemhighpriority ? "fuego" : "hielo"}}
-  <ul>
-    <li v-for="{ id, label } in items" v-bind:key="id">🔹 {{label}}</li>
-  </ul>
-</template>
-
-<style scoped>
-.shopping-cart-icon {
-  font-size: 2rem;
-}
-</style>
+  </button> 
+  </form> 
+  <ul> 
+    <li v-for="{ id, label } in items" v-bind:key="id"> 🔹 {{ label }} </li> 
+    </ul> </template> 
+    <style scoped> 
+    .shopping-cart-icon { font-size: 2rem; /* Adjust the font-size value as per your desired size */ } 
+    </style>
