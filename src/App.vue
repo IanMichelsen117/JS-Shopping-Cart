403682ceb1834cb517f967c8df174d16e9435b85
IanMichelsen117 <138816928+IanMichelsen117@users.noreply.github.com>
2023-10-09 18:49:30 +0000

    save

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 import { ref } from 'vue'; 
 const header = ref('App Lista de compras'); 
 const items = ref([ 
-  {id: 1, label: '10 bolillos'},
-  {id: 2, label: '1 lata de frijoles'}, 
-  {id: 3, label: '2 lata de atún'} ]); 
+  // {id: 1, label: '10 bolillos'},
+  // {id: 2, label: '1 lata de frijoles'}, 
+  // {id: 3, label: '2 lata de atún'} 
+]); 
   //Agregando metodo para guardar nuevo articulo en la lista
 const saveItem=() =>{
 items.value.push({id: items.value.length + 1, label: newItem.value})
@@ -15,7 +16,13 @@ const newItem = ref('');
 const newItemHighPriority = ref(false); 
 </script> 
 
-<template> <form v-on:submit.prevent="saveItem" class="add-item form"> 
+<template> 
+<div class="header">
+  <h1> <i class="material-icons shopping-cart-icon">local_mall </i>{{ header }}</h1>
+  <button class="btn">Cancelar</button>
+  <button class="btn btn-primary">Agregar Articulo</button>
+</div>
+<form v-on:submit.prevent="saveItem" class="add-item form"> 
 <!-- Input de Nuevo Articulo --> 
 <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo"> 
 <!-- Check Boxes --> 
@@ -29,8 +36,13 @@ const newItemHighPriority = ref(false);
   </button> 
   </form> 
   <ul> 
-    <li v-for="{ id, label } in items" v-bind:key="id"> 🔹 {{ label }} </li> 
-    </ul> </template> 
+    <li v-for="{ id, label } in items" v-bind:key="id"> 
+      🔹 {{ label }} 
+    </li> 
+    </ul> 
+    <p v-if="items.length == 0">🥀 Lista de Compras Vacia 🥀 </p>
+    <p v-else >🔥 Ingrese mas  items 🔥 </p>
+    </template> 
     <style scoped> 
     .shopping-cart-icon { font-size: 2rem; /* Adjust the font-size value as per your desired size */ } 
     </style>
